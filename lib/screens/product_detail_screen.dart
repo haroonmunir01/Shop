b66dc360096263672a0fdd8a2c38edee647a2809
haroonmunir01 <haroonmunir2002@gmail.com>
2024-10-23 02:06:09 +0500

    Single StateMangement Done at Multiple Places

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../providers/products.dart';
 
 class ProductDetailScreen extends StatelessWidget {
   const ProductDetailScreen({super.key});
@@ -7,8 +9,9 @@ static const routeName='/product-detail';
   @override
   Widget build(BuildContext context) {
     final productId=ModalRoute.of(context)!.settings.arguments as String;
+    final loadedProduct=Provider.of<Products>(context,listen: false).findById(productId);
     return Scaffold(
-      appBar: AppBar(title: Text(' Detail....'),),
+      appBar: AppBar(title: Text(loadedProduct.title!),),
     );
   }
 }
